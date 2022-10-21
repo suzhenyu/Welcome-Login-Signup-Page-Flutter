5a5270024f91971c7543f91641dc43f56fac1807
suzhenyu <983424749@qq.com>
2022-10-21 18:16:23 +0800

    实现了首页，只不过两个按钮的宽度还是有问题

---
+++ flutter_auth/lib/Screens/Welcome/login_and_signup_btn.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/constants.dart';
+
+class LoginAndSignupBtn extends StatelessWidget {
+  const LoginAndSignupBtn({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {},
+          style: const ButtonStyle(
+            foregroundColor: MaterialStatePropertyAll<Color>(Colors.white),
+            backgroundColor: MaterialStatePropertyAll<Color>(Color(0xFF6F35A5)),
+          ),
+          child: Text(
+            "Login".toUpperCase(),
+          ),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kPrimaryLightColor, elevation: 0),
+          child: Text(
+            "Sign up".toUpperCase(),
+            style: const TextStyle(color: Colors.black),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ flutter_auth/lib/Screens/Welcome/welcome.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Welcome/login_and_signup_btn.dart';
+import 'package:flutter_auth/Screens/Welcome/welcome_image.dart';
+import 'package:flutter_auth/components/background.dart';
+import 'package:flutter_auth/responsive.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Background(
+      child: SingleChildScrollView(
+        child: SafeArea(
+          child: Responsive(
+            mobile: MobileWelcomeScreen(),
+            desktop: MobileWelcomeScreen(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DesktopWelcomeScreen extends StatelessWidget {
+  const DesktopWelcomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        const Expanded(
+          child: WelcomeImage(),
+        ),
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              SizedBox(
+                width: 450,
+                child: LoginAndSignupBtn(),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MobileWelcomeScreen extends StatelessWidget {
+  const MobileWelcomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const WelcomeImage(),
+        Row(
+          children: const [
+            Spacer(),
+            Expanded(
+              flex: 8,
+              child: LoginAndSignupBtn(),
+            ),
+            Spacer(),
+          ],
+        ),
+      ],
+    );
+  }
+}
